fbcaccc907131387e7bf433840da9f748ae3c298
Haroldo de Oliveira Pinheiro <haroldoop@gmail.com>
2015-09-17 20:51:27 -0300

    Made the build script work correctly

--- build.bat
+++ build.bat
@@ -3,7 +3,7 @@ echo Build gfx.c from gfx folder
 folder2c gfx gfx
 sdcc -c -mz80 --std-sdcc99 gfx.c
 echo Build Main
-sdcc -c -mz80 --std-sdcc99 sms-rogue.c
+sdcc -c -mz80 --std-sdcc99 txtelite.c
 echo Linking
-sdcc -mz80 --data-loc 0xC000 sms-rogue.rel SMSlib/SMSlib.rel gfx.rel
-ihx2sms sms-rogue.ihx sms-rogue.sms
+sdcc -mz80 --data-loc 0xC000 txtelite.rel SMSlib/SMSlib.rel gfx.rel
+ihx2sms txtelite.ihx txtelite.sms
